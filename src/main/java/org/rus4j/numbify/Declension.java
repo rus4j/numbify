8031dee7bd385a5bfc06066809e74bdf432c92ef
rus4j <eto_ya08@mail.ru>
2025-02-27 20:36:53 +0300

    add Declension java doc

--- src/main/java/org/rus4j/numbify/Declension.java
+++ src/main/java/org/rus4j/numbify/Declension.java
@@ -1,4 +1,10 @@
 package org.rus4j.numbify;
 
+/**
+ * Some languages could have different number text representation depends on declension.
+ * For example there are 6 declensions in russian
+ * (see {@link org.rus4j.numbify.lang.ru.RuDeclension}),
+ * but only 1 in english (see {@link org.rus4j.numbify.lang.en.EnDeclension}).
+ */
 public interface Declension {
 }
