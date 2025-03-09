657b88bbb727c7c18b2f713e0fb6cc58782e099d
rus4j <eto_ya08@mail.ru>
2025-03-09 22:42:39 +0300

    add Currency enum and lang dictionaries for all supported currencies

---
+++ src/main/java/org/rus4j/numbify/lang/en/EnCurrencyDictionary.java
@@ -0,0 +1,32 @@
+package org.rus4j.numbify.lang.en;
+
+import org.rus4j.numbify.Currency;
+
+public class EnCurrencyDictionary {
+
+    final String currency(Currency currency, boolean pluralForm) {
+        int form = pluralForm ? 1 : 0;
+        return switch (currency) {
+            case RUB -> ruble[form];
+            case USD -> usd[form];
+            case EUR -> euro[form];
+            case NO_CURRENCY -> "";
+        };
+    }
+
+    final String decimalCurrency(Currency currency, boolean pluralForm) {
+        int form = pluralForm ? 1 : 0;
+        return switch (currency) {
+            case RUB -> decimalRub[form];
+            case USD, EUR -> cent[form];
+            case NO_CURRENCY -> "";
+        };
+    }
+
+    final String[] ruble = new String[]{"ruble", "rubles"};
+    final String[] usd = new String[]{"dollar", "dollars"};
+    final String[] euro = new String[]{"euro", "euros"};
+
+    final String[] decimalRub = new String[]{"kopeck", "kopecks"};
+    final String[] cent = new String[]{"cent", "cents"};
+}
