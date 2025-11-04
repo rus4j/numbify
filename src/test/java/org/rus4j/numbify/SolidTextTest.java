c0abe97f8370b6f51c58b8712ef04dc603345402
rus4j <eto_ya08@mail.ru>
2025-11-04 17:37:08 +0300

    more tests

--- src/test/java/org/rus4j/numbify/SolidTextTest.java
+++ src/test/java/org/rus4j/numbify/SolidTextTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.rus4j.numbify.lang.Currency;
 import org.rus4j.numbify.lang.Language;
 import org.rus4j.numbify.lang.en.English;
+import org.rus4j.numbify.lang.ru.Russian;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,10 +18,31 @@ public class SolidTextTest {
 
         assertThat(solid.toText(123)).isEqualTo("onehundredtwentythree");
     }
+
+    @Test
+    public void solidRussianTest() {
+        Numbify solid = new Numbify(
+                new CustomRussian(Currency.NUMBER),
+                new IntText(new SolidText())
+        );
+
+        assertThat(solid.toText(123)).isEqualTo("стодвадцатьтри");
+    }
 }
 
 class CustomEnglish extends English {
     public CustomEnglish(Currency currency) {
+        super(currency);
+    }
+
+    @Override
+    public DigitGroupOrder textOrder() {
+        return new ForwardOrder("");
+    }
+}
+
+class CustomRussian extends Russian {
+    public CustomRussian(Currency currency) {
         super(currency);
     }
 
