3c2013544965ee4daad966596a0f8e97e020053c
vodobryshkin <dobryshkinvladimir@yandex.ru>
2026-08-04 00:39:02 +0300

    Rewrote the tests in the form adopted in the project and removed AAA comments

--- src/test/java/org/rus4j/numbify/NumbifyBuilderTest.java
+++ src/test/java/org/rus4j/numbify/NumbifyBuilderTest.java
@@ -12,141 +12,77 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class NumbifyBuilderTest {
     @Test
     void digitByDigitIntTest() {
-        // Arrange
-        double value = 123.12;
-
-        Numbify sut = new NumbifyBuilder()
+        Numbify en = new NumbifyBuilder()
                 .english()
                 .digitByDigitInt()
                 .build();
-
-        // Act
-        String actual = sut.toText(value);
-
-        // Assert
-        assertThat(actual).isEqualTo("one two three dollars twelve cents");
+        assertThat(en.toText(123.12)).isEqualTo("one two three dollars twelve cents");
     }
 
     @Test
     void solidIntTest() {
-        // Arrange
-        double value = 123.12;
-
-        Numbify sut = new NumbifyBuilder()
+        Numbify en = new NumbifyBuilder()
                 .english()
                 .solidInt()
                 .build();
-
-        // Act
-        String actual = sut.toText(value);
-
-        // Assert
-        assertThat(actual).isEqualTo("one hundredtwenty-three dollars twelve cents");
+        assertThat(en.toText(123.12)).isEqualTo("one hundredtwenty-three dollars twelve cents");
     }
 
     @Test
     void customIntTextEngineTest() {
-        // Arrange
-        double value = 123.12;
-
-        Numbify sut = new NumbifyBuilder()
+        Numbify en = new NumbifyBuilder()
                 .english()
                 .customIntTextEngine((number, lang, isDecimal) -> "custom")
                 .build();
-
-        // Act
-        String actual = sut.toText(value);
-
-        // Assert
-        assertThat(actual).isEqualTo("custom dollars twelve cents");
+        assertThat(en.toText(123.12)).isEqualTo("custom dollars twelve cents");
     }
 
     @Test
     void digitByDigitDecimalTest() {
-        // Arrange
-        double value = 123.12;
-
-        Numbify sut = new NumbifyBuilder()
+        Numbify en = new NumbifyBuilder()
                 .english()
                 .digitByDigitDecimal()
                 .build();
-
-        // Act
-        String actual = sut.toText(value);
-
-        // Assert
-        assertThat(actual).isEqualTo("one hundred twenty-three dollars one two cents");
+        assertThat(en.toText(123.12)).isEqualTo("one hundred twenty-three dollars one two cents");
     }
 
     @Test
     void solidDecimalTest() {
-        // Arrange
-        double value = 123.123;
-
-        Numbify sut = new NumbifyBuilder()
+        Numbify en = new NumbifyBuilder()
                 .english(Currency.NUMBER)
                 .solidDecimal()
                 .build();
-
-        // Act
-        String actual = sut.toText(value);
-
-        // Assert
-        assertThat(actual).isEqualTo("one hundred twenty-three one hundredtwenty-three thousandths");
+        assertThat(en.toText(123.123)).isEqualTo("one hundred twenty-three one hundredtwenty-three thousandths");
     }
 
     @Test
     void customDecimalTextEngineTest() {
-        // Arrange
-        double value = 123.12;
-
-        Numbify sut = new NumbifyBuilder()
+        Numbify en = new NumbifyBuilder()
                 .english()
                 .customDecimalTextEngine((number, lang, isDecimal) -> "custom")
                 .build();
-
-        // Act
-        String actual = sut.toText(value);
-
-        // Assert
-        assertThat(actual).isEqualTo("one hundred twenty-three dollars custom cents");
+        assertThat(en.toText(123.12)).isEqualTo("one hundred twenty-three dollars custom cents");
     }
 
     @Test
     void severalIntTextEnginesInARowTest() {
-        // Arrange
-        double value = 123.12;
-
-        Numbify sut = new NumbifyBuilder()
+        Numbify en = new NumbifyBuilder()
                 .english()
                 .digitByDigitInt()
                 .solidInt()
                 .customIntTextEngine((number, lang, isDecimal) -> "custom")
                 .build();
-
-        // Act
-        String actual = sut.toText(value);
-
-        // Assert
-        assertThat(actual).isEqualTo("custom dollars twelve cents");
+        assertThat(en.toText(123.12)).isEqualTo("custom dollars twelve cents");
     }
 
     @Test
     void severalDecimalTextEnginesInARowTest() {
-        // Arrange
-        double value = 123.12;
-
-        Numbify sut = new NumbifyBuilder()
+        Numbify en = new NumbifyBuilder()
                 .english()
                 .digitByDigitDecimal()
                 .solidDecimal()
                 .customDecimalTextEngine((number, lang, isDecimal) -> "custom")
                 .build();
-
-        // Act
-        String actual = sut.toText(value);
-
-        // Assert
-        assertThat(actual).isEqualTo("one hundred twenty-three dollars custom cents");
+        assertThat(en.toText(123.12)).isEqualTo("one hundred twenty-three dollars custom cents");
     }
 }
