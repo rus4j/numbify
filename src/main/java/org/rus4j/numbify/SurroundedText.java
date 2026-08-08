6a86be1277f96bc8e1f5a11e7efb7ff052e8f08b
vodobryshkin <dobryshkinvladimir@yandex.ru>
2026-08-08 15:20:54 +0300

    Implement SurroundedText

---
+++ src/main/java/org/rus4j/numbify/SurroundedText.java
@@ -0,0 +1,23 @@
+package org.rus4j.numbify;
+
+import org.rus4j.numbify.lang.Language;
+import org.rus4j.numbify.number.StringNumber;
+
+public class SurroundedText implements NumberText {
+    private final NumberText numberText;
+    private final String prefix;
+    private final String suffix;
+
+    public SurroundedText(NumberText numberText, String prefix, String suffix) {
+        this.numberText = numberText;
+        this.prefix = prefix;
+        this.suffix = suffix;
+    }
+
+    @Override
+    public String toText(StringNumber number, Language language) {
+        String text = numberText.toText(number, language);
+
+        return text.isEmpty() ? "" : prefix + text + suffix;
+    }
+}
